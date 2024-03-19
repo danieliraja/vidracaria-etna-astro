a7b54e416c21b26e7f81a4742a8d35655658c8c8
Daniel Sousa <daniel.sousa@msn.com>
2024-03-19 19:54:43 -0300

    configurando mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://danieliraja.github.io",
+  base: "/vidracaria-etna-astro",
+});
