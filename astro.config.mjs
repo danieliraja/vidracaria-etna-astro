ad4fbea13099a00be7adbeb3029cd60227e61488
Daniel Sousa <daniel.sousa@msn.com>
2024-04-17 22:01:01 -0300

    prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,30 @@
 import { defineConfig } from "astro/config";
 import swup from "@swup/astro";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://etnavidros.com.br",
-  base: "/",
-  integrations: [
-    swup({
-      theme: "slide",
-      animationClass: "transition-",
-      containers: ["main"],
-      cache: true,
-      preload: true,
-      accessibility: true,
-      forms: false,
-      morph: false,
-      parallel: false,
-      progress: false,
-      routes: false,
-      smoothScrolling: true,
-      updateBodyClass: false,
-      updateHead: true,
-      reloadScripts: true,
-      debug: false,
-      loadOnIdle: true,
-      globalInstance: false,
-      smoothScrolling: true,
-    }),
-  ],
+  site: "https://danieliraja.github.io",
+  base: "/vidracaria-etna-astro",
+  integrations: [swup({
+    theme: "slide",
+    animationClass: "transition-",
+    containers: ["main"],
+    cache: true,
+    preload: true,
+    accessibility: true,
+    forms: false,
+    morph: false,
+    parallel: false,
+    progress: false,
+    routes: false,
+    smoothScrolling: true,
+    updateBodyClass: false,
+    updateHead: true,
+    reloadScripts: true,
+    debug: false,
+    loadOnIdle: true,
+    globalInstance: false,
+    smoothScrolling: true
+  }), sitemap()]
 });
